b1a92f9024c74f73278e1ccc61b98eadcbce6b13
Sobirjon Pirmatov <sobirjon.0513@gmail.com>
2025-09-10 21:35:59 +0900

    created Title.jsx and FeaturedDestination.jsx files

--- src/components/HotelCard.jsx
+++ src/components/HotelCard.jsx
@@ -5,10 +5,10 @@ import { assets } from '../assets/assets';
 const HotelCard = ({ room, index }) => {
     return (
         <Link to={'/rooms/' + room._id}
-            onClick={() => scrollTo(0, 0)} key={room._id}>
-            <img src={room.images[0]} alt=""
-            className='relative max-w-70 w-full rounded-xl overflow-hidden bg-white text-gray-500/90 shadow-[0px_4px_4px_fgba(0,0,0,0.05)]' />
-
+            onClick={() => scrollTo(0, 0)} key={room._id}
+            className='relative max-w-70 w-full rounded-xl overflow-hidden bg-white text-gray-500/90 shadow-[0px_4px_4px_fgba(0,0,0,0.05)]' >
+            <img src={room.images[0]} alt="" />
+ 
             {index % 2 === 0 && <p className='px-3 py-1 absolute top-3 left-3 text-xs bg-white text-gray-800 font-medium rounded-full'>
                 Best Hotels
             </p>}
@@ -17,15 +17,18 @@ const HotelCard = ({ room, index }) => {
                     <p className='font-playfair text-xl font-medium text-gray-800'>
                         {room.hotel.name}</p>
                     <div className='flex-items-center gap-1'>
-                        <img src={assets.starIconFilled} alt="star-icon" /> 4.5
+                        <img src={assets.starIconFilled} alt="star-icon" /> 5.0
                     </div>
                 </div>
-                <div>
+                <div className='flex items-center gap-1 text-sm'>
                     <img src={assets.locationIcon} alt="location-icon" />
                     <span>{room.hotel.address}</span>
                 </div>
-                <div>
-                    <p><span>${room.pricePerNight} </span>/night</p>
+                <div className='flex items-center justify-between mt-4'>
+                    <p><span className='text-xl text-gray-800'>${room.pricePerNight} </span>/night</p>
+                    <button className='px-4 py-2 text-sm font-medium border border-gray-300 rounded hover:bg-gray-50 transition-all cursor-pointer'>
+                        Book Now
+                    </button>
                 </div>
             </div>
         </Link>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import FeaturedDestination from "../components/FeaturedDestination";
 
 const HomePage = () => {
    return(
       <div>
        <Header/>
+       <FeaturedDestination/>
       </div>
    )
 }

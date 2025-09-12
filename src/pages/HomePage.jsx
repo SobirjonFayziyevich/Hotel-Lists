4bc76374b06b7a1a7b176a1fe0b9c00057c416fc
Sobirjon Pirmatov <sobirjon.0513@gmail.com>
2025-09-12 20:51:33 +0900

    created ExclusiveOffers.jsx component file

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Header from "../components/Header";
 import FeaturedDestination from "../components/FeaturedDestination";
+import ExclusiveOffers from "../components/ExclusiveOffers";
 
 const HomePage = () => {
    return(
       <div>
-       <Header/>
-       <FeaturedDestination/>
-      </div>
+       <Header />
+       <FeaturedDestination />
+       <ExclusiveOffers/>
+
+       </div>
    )
 }
-export default HomePage;
+export default HomePage; 
